baeef280ca1e07b3de17c63c7f936b9969e8fb54
Maximilien Marie <contact@mmarie.fr>
2018-10-29 16:54:24 +0100

    feat(auth): enable auth for production

--- frontend/src/app/utils/app-init.ts
+++ frontend/src/app/utils/app-init.ts
@@ -1,12 +1,17 @@
 import {KeycloakService} from 'keycloak-angular';
+import {environment} from '../../environments/environment';
 
 export function initializer(keycloak: KeycloakService): () => Promise<any> {
-  // return (): Promise<any> => keycloak.init({config: environment.keycloak, initOptions: {
-  //     onLoad: 'login-required',
-  //     checkLoginIframe: false
-  //   }});
-
-  return (): Promise<any> => new Promise((resolve, reject) => {
-    resolve();
-  });
+  if (environment.production) {
+    return (): Promise<any> => keycloak.init({
+      config: environment.keycloak, initOptions: {
+        onLoad: 'login-required',
+        checkLoginIframe: false
+      }
+    });
+  } else {
+    return (): Promise<any> => new Promise((resolve) => {
+      resolve();
+    });
+  }
 }
