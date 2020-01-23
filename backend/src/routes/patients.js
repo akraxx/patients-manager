6c24f648c119e636056c6c406f63588f5f7b9f72
Maximilien Marie <contact@mmarie.fr>
2020-01-23 19:46:57 +0100

    feat: add pdf dependencies + example

--- backend/src/routes/patients.js
+++ backend/src/routes/patients.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+let ejs = require("ejs");
+let pdf = require("html-pdf");
+let fs = require("fs");
+let path = require("path");
+
 var Patient = require('../models/Patient');
 
 function sortTypeToNumber(sortType) {
@@ -78,5 +83,79 @@ router.put('/:id', function (req, res, next) {
         });
     });
 });
+
+/* UPDATE PATIENT */
+router.get('/:id/consultations/:consultationId/invoice', function (req, res, next) {
+
+    /*var html = fs.readFileSync('./data/invoice-template.html', 'utf8');
+    var options = {
+        format: 'Letter'
+    };
+
+    console.log(html);
+    pdf.create(html, options).toFile('./data/businesscard.pdf', function(err, data) {
+        console.log(data);
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("File created successfully");
+        }
+    });*/
+
+    let students = [
+        {name: "Joy",
+            email: "joy@example.com",
+            city: "New York",
+            country: "USA"},
+        {name: "John",
+            email: "John@example.com",
+            city: "San Francisco",
+            country: "USA"},
+        {name: "Clark",
+            email: "Clark@example.com",
+            city: "Seattle",
+            country: "USA"},
+        {name: "Watson",
+            email: "Watson@example.com",
+            city: "Boston",
+            country: "USA"},
+        {name: "Tony",
+            email: "Tony@example.com",
+            city: "Los Angels",
+            country: "USA"
+        }];
+
+    ejs.renderFile(path.join(__dirname, '../../data/', "invoice-template.ejs"), {students: students}, (err, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            let options = {
+                "height": "11.25in",
+                "width": "8.5in",
+                "header": {
+                    "height": "20mm"
+                },
+                "footer": {
+                    "height": "20mm",
+                },
+            };
+            pdf.create(data, options).toStream(function (err, stream) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    var filename = "invoice.pdf";
+                    var mimetype = "application/pdf";
+
+                    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+                    res.setHeader('Content-type', mimetype);
+
+                    stream.pipe(res);
+                }
+            });
+        }
+    });
+    //res.send("test");
+});
+
 
 module.exports = router;
